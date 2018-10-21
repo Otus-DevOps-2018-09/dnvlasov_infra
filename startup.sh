3384aa9a3fb0d9bbbbf1cda3d0a18eff016818ac
Dmitrii Vlasov <dnvlasov@gmail.com>
2018-10-21 18:37:05 +0300

    change file  startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 #install ruby
 sudo apt update
@@ -6,7 +6,7 @@ sudo apt install -y ruby-full ruby-bundler build-essential
 
 #install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu \
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu\
 	xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
@@ -14,6 +14,7 @@ sudo systemctl start mongod
 sudo systemctl enable mongod
 
 #deploy puma
+cd /home/appuser
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
