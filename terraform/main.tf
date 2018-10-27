54490e837bb4a0e5ff6990df28638800a1a9686e
Dmitrii Vlasov <dnvlasov@gmail.com>
2018-10-27 20:19:46 +0300

    Change main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ metadata {
  }
 connection {
  type = "ssh"
- user = "appuser2"
+ user = "appuser1"
  agent = false
  private_key = "${file(var.private_key)}"
  }
@@ -44,13 +44,14 @@ resource "google_compute_firewall" "firewall_puma" {
  source_ranges = ["0.0.0.0/0"]
  target_tags = ["reddit-app"]
 } 
-resource "google_compute_project_metadata" "app_appuser3" {
-metadata {
-ssh-keys = "appuser3:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZHv7EnLpujeP8LJxt9BxZbqk70k8izrjJw5aQT+s8trb5gPGd1hmANonjJNr03HQPfyvsAXvzDPNnUmqwxIbS5WyfXMFQz7MaaetDTFDZl0LzXcm4HBnrj5lkEhdR5+wTotdQQw7OFf0sCta0MV5hDQ1x/+4P7ojDn2YcXbsVe1Zsx/HYDXAeGXW43SmIgo6vwTTTtU6HgeNps5fi5jhivQQd9aSG5AKwvm3uzyMce0j9VovUf+j0a/WC0IllWURVUpwozP2/iCtIJ9jhiQsluU9eNcR6RoJaI/ZN3y+u0PGhYY2H5UKfdgfWKjTzPTubOtw2Db+RVsJ8SJ7feyvx appuser3"  
-}
-}
-resource "google_compute_project_metadata" "app_appuser1" {
-metadata {
-ssh-keys = "appuser1:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNZaLRYqJ7vb3V0UfuorMveOuwRtTjbT4tBkEeDbfNGPHocmTg1sYp9G6Lpx53FjnTderr1/eZdYSGAvmT4t286N/URgqEhh585bXMZ+FX+I5CAiNSYAVBWAYvlDU/r1q8oINK59APxGKk/+suavwrfSKcvA3dxtVNxnfMhGuEWfQL9GbCslzanGZHxoSszIa0iS585pJoKVZL2ffzg8Bg3rFZ6AnK+czQOpBpsd+VFGLz0tbxcWL05srf4TNH4ljbFyLuR9DObVEJ7F/+SfKXf2E1jEoZeMW82u+cYo5lZBzaJt/EQMfEvS3/e/6Pb2dm5JfyBcY9mkKRSRkTKDPB appuser1"
-}
+resource "google_compute_project_metadata" "ssh_keys" {
+    metadata {
+      ssh-keys = <<EOF
+      appuser1:${var.public_key_path} appuser1
+      appuser2:${var.public_key_path} appuser2
+      appuser3:${var.public_key_path} appuser3
+
+		
+EOF
+    }
 }
